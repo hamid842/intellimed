e53af102ef8fe12a6ea93086ac565de34120b4d9
mehdi <mjv703@yahoo.com>
2021-05-22 20:24:37 +0430

    Structure is modified

--- src/main/app/modules/pages/dashboard/Dashboard.jsx
+++ src/main/app/modules/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 
-//import CurrentMedication from "@pages/dashboard/current-medication/CurrentMedication";
 import AppCalendar from "@pages/dashboard/calendar/AppCalendar";
-//import SideEffectQuestions from "@pages/dashboard/sideEffect-questions/SideEffectQuestions";
-//import Prescriber from "@pages/dashboard/prescriber/Prescriber";
-//import AddNewPrescription from "@pages/dashboard/add-prescription/AddNewPrescription";
+import CurrentMedication from "@pages/dashboard/recent-prescription/RecentPrescription";
 
 const Dashboard = () => {
   return (
@@ -17,15 +14,6 @@ const Dashboard = () => {
         </Grid>
         <Grid item xs={12} sm={8} lg={8}>
           <AppCalendar />
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={6} lg={6}>
-              {/* <Prescriber /> */}
-            </Grid>
-            <Grid item xs={12} sm={6} lg={6}>
-              {" "}
-              {/* <AddNewPrescription /> */}
-            </Grid>
-          </Grid>
         </Grid>
       </Grid>
     </>
